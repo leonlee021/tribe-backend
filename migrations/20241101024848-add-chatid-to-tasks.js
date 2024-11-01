dd683f973548ac8a0982584cf0c1153b74824bac
Leon Lee <leonl.lee@mail.utoronto.ca>
2024-10-31 22:50:19 -0400

    Add migration for chatId in Task model

---
+++ migrations/20241101024848-add-chatid-to-tasks.js
@@ -0,0 +1,16 @@
+// migrations/20241101-add-chatId-to-Task.js
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Tasks', 'chatId', {
+      type: Sequelize.INTEGER,
+      allowNull: true, // or false based on your requirements
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Tasks', 'chatId');
+  }
+};
